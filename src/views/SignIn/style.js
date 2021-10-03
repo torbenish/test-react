bdc2c2db48c7874a1dbf8fe8b9bf4897fc80fc04
Ishtrb <ishiitorben@gmail.com>
2021-10-03 19:54:31 -0300

    :lipstick: Change color

--- src/views/SignIn/style.js
+++ src/views/SignIn/style.js
@@ -6,7 +6,7 @@ export const Container = styled.div`
   align-items: center;
   width: 100%;
   height: 100vh;
-  background-color: red;
+  background-color: yellow;
   color: #fff;
   .form-wrapper {
     display: grid;
